82ffe33a5eb7c6c7035eb2cccb4342fb90e4435d
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-03-15 11:23:48 +0100

    fix linter error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
 export default class App extends React.PureComponent {
-
-  render(){
-return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
   }
-  
 }
